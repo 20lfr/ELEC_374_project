d86c480fbd4598f5a4cae05229c740976ef5f0e5
Luka <lukarod@icloud.com>
2024-01-25 21:57:25 -0500

    added test bench provided in phase 1 report. Requires further investigation

--- 374_LAB_system/ALU_files/ALU.v
+++ 374_LAB_system/ALU_files/ALU.v
@@ -1,12 +1,15 @@
-module ALU(
-	input wire [7:0] A, B, 
-	input wire [3:0] op, 
-	output reg[7:0] result
+module ALU #(parameter DATA_WIDTH = 32)(
+	input wire [DATA_WIDTH - 1:0] A, B, 
+	input wire [3:0] op, //this is represented as 4 bits becuase we have just less than 16 operations (aka 2^4)
+	output reg[(DATA_WIDTH*2)-1:0] result
 	
 );
+
+//regular operations: AND, OR, XOR, NOT, Shift left, Shift Right, shra, ror, rol, neg
+//register operations: load, store, move, 
+//arithmatic operations: add, sub, multiply, divide
 	
-	
-	wire [7:0] and_result, or_result;
+	wire [31:0] and_result, or_result;
 	
 	and_or and_instance(A, B, 1, and_result);
 	and_or or_instance(A, B, 0, or_result);
