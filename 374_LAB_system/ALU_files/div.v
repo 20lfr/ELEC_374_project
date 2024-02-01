550b589a104f43c4b3710f97b8eedd64d55a3fdf
michaeljborg <michaelborg99@hotmail.com>
2024-02-01 11:29:18 -0500

    div.v update

--- 374_LAB_system/ALU_files/div.v
+++ 374_LAB_system/ALU_files/div.v
@@ -1,1 +1,32 @@
-Nah I'd win
+// Nah I'd Win
+
+module div(
+
+    input wire [31:0] A,                    // Dividend
+    input wire [31:0] B,                    // Divisor
+    output wire [63:0] div_result           // Bottom 32-bits will be Quotient, Top 32-bits will be remainder 
+
+);
+
+always @(A) begin
+
+    Quotient = 0;
+    Remainder = 0;
+    temp = 0;
+
+    if (B != 0) begin 
+
+        // Division Algorithm
+
+        
+
+    end 
+
+    else begin
+        
+        // Handle dividing by 0
+    
+    end
+end
+
+endmodule
