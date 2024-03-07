be0e7974f7268c638cd0182b1d08ae3c6902bbfb
Michael <101535883+michaeljborg@users.noreply.github.com>
2024-03-07 18:29:31 -0500

    Update Inport_reg.v
    fix spaces

--- System/DataPath_partition/Registers/Inport_reg.v
+++ System/DataPath_partition/Registers/Inport_reg.v
@@ -8,21 +8,21 @@ reg [DATA_WIDTH_IN-1:0]q;
 reg strobe_prev;
 
 initial begin
-    q = INIT;
-    strobe_prev = 1'b0;
+	q = INIT;
+	strobe_prev = 1'b0;
 end
 
 always @ (posedge clock)
 		begin 
 			if (clear) begin
 				q <= {DATA_WIDTH_IN{1'b0}};
-                strobe_prev <= 1'b0;
+                		strobe_prev <= 1'b0;
 			end
 			else begin
-                strobe_prev <= strobe;
-                if (strobe && !strobe_prev) begin
-				    q <= External_Input;
-                end
+                		strobe_prev <= strobe;
+               	 		if (strobe && !strobe_prev) begin
+					q <= External_Input;
+                		end
 			end
 		end
 	assign BusMuxIn = q[DATA_WIDTH_OUT-1:0];
